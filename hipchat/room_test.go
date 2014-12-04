3900df424ab964cfe47f7dbe85bd007a6acaac6a
Tobias Schmid <toashd@gmail.com>
2014-12-04 03:50:49 +0100

    Add share file with rooms and users
    Add file upload request
    
    Add share file with room
    
    Add share file with user
    
    Add file sharing example

--- hipchat/room_test.go
+++ hipchat/room_test.go
@@ -6,6 +6,8 @@ import (
 	"net/http"
 	"reflect"
 	"testing"
+	"io/ioutil"
+	"os"
 )
 
 func TestRoomGet(t *testing.T) {
@@ -18,8 +20,8 @@ func TestRoomGet(t *testing.T) {
 		}
 		fmt.Fprintf(w, `
 		{
-			"id":1, 
-			"name":"n", 
+			"id":1,
+			"name":"n",
 			"links":{"self":"s"},
 			"Participants":[
 				{"Name":"n1"},
@@ -55,7 +57,7 @@ func TestRoomList(t *testing.T) {
 		}
 		fmt.Fprintf(w, `
 		{
-			"items": [{"id":1,"name":"n"}], 
+			"items": [{"id":1,"name":"n"}],
 			"startIndex":1,
 			"maxResults":1,
 			"links":{"Self":"s"}
@@ -94,6 +96,45 @@ func TestRoomNotification(t *testing.T) {
 	_, err := client.Room.Notification("1", args)
 	if err != nil {
 		t.Fatalf("Room.Notification returns an error %v", err)
+	}
+}
+
+func TestRoomShareFile(t *testing.T) {
+	setup()
+	defer teardown()
+
+	tempFile, err := ioutil.TempFile(os.TempDir(), "hipfile")
+	tempFile.WriteString("go gophers")
+	defer os.Remove(tempFile.Name())
+
+	want := "--hipfileboundary\n" +
+		"Content-Type: application/json; charset=UTF-8\n" +
+		"Content-Disposition: attachment; name=\"metadata\"\n\n" +
+		"{\"message\": \"Hello there\"}\n" +
+		"--hipfileboundary\n" +
+		"Content-Type:  charset=UTF-8\n" +
+		"Content-Transfer-Encoding: base64\n" +
+		"Content-Disposition: attachment; name=file; filename=hipfile\n\n" +
+		"Z28gZ29waGVycw==\n" +
+		"--hipfileboundary\n"
+
+	mux.HandleFunc("/room/1/share/file", func(w http.ResponseWriter, r *http.Request) {
+		if m := "POST"; m != r.Method {
+			t.Errorf("Request method %s, want %s", r.Method, m)
+		}
+
+		body, _ := ioutil.ReadAll(r.Body)
+
+		if string(body) != want {
+			t.Errorf("Request body \n%+v\n,want \n\n%+v", string(body), want)
+		}
+		w.WriteHeader(http.StatusNoContent)
+	})
+
+	args := &ShareFileRequest{Path: tempFile.Name(), Message: "Hello there", Filename: "hipfile"}
+	_, err = client.Room.ShareFile("1", args)
+	if err != nil {
+		t.Fatalf("Room.ShareFile returns an error %v", err)
 	}
 }
 
--- hipchat/user.go
+++ hipchat/user.go
@@ -1,5 +1,10 @@
 package hipchat
 
+import (
+	"fmt"
+	"net/http"
+)
+
 // User represents the HipChat user.
 type User struct {
 	ID          int    `json:"id"`
@@ -7,3 +12,21 @@ type User struct {
 	MentionName string `json:"mention_name"`
 	Links       Links  `json:"links"`
 }
+
+// UserService gives access to the user related methods of the API.
+type UserService struct {
+	client *Client
+}
+
+// ShareFile sends a file to the user specified by the id.
+//
+// HipChat API docs: https://www.hipchat.com/docs/apiv2/method/share_file_with_user
+func (u *UserService) ShareFile(id string, shareFileReq *ShareFileRequest) (*http.Response, error) {
+	req, err := u.client.NewFileUploadRequest("POST", fmt.Sprintf("user/%s/share/file", id), shareFileReq)
+	if err != nil {
+		return nil, err
+	}
+
+	return u.client.Do(req, nil)
+}
+
