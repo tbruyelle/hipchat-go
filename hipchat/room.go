ec47fd6815c355fa5045c6c6b5773dd7c0ccea5a
Adam Bachman <adam.bachman@gmail.com>
2014-12-04 11:34:56 -0500

    move webhook methods into room_webhook.go

--- hipchat/room.go
+++ hipchat/room.go
@@ -178,59 +178,6 @@ func (r *RoomService) Update(id string, roomReq *UpdateRoomRequest) (*http.Respo
 	return r.client.Do(req, nil)
 }
 
-// List all webhooks for a given room.
-//
-// HipChat API docs: https://www.hipchat.com/docs/apiv2/method/get_all_webhooks
-func (r *RoomService) GetAllWebhooks(id interface{}, roomReq *GetAllWebhooksRequest) (*WebhookList, *http.Response, error) {
-	req, err := r.client.NewRequest("GET", fmt.Sprintf("room/%v/webhook", id), roomReq)
-	if err != nil {
-		return nil, nil, err
-	}
-	whList := new(WebhookList)
-
-	resp, err := r.client.Do(req, whList)
-	if err != nil {
-		return nil, resp, err
-	}
-	return whList, resp, nil
-}
-
-// Delete a given webhook.
-//
-// HipChat API docs: https://www.hipchat.com/docs/apiv2/method/delete_webhook
-func (r *RoomService) DeleteWebhook(id interface{}, webhookId interface{}) (*http.Response, error) {
-	req, err := r.client.NewRequest("DELETE", fmt.Sprintf("room/%v/webhook/%v", id, webhookId), nil)
-	if err != nil {
-		return nil, err
-	}
-
-	resp, err := r.client.Do(req, nil)
-	if err != nil {
-		return resp, err
-	}
-
-	return resp, nil
-}
-
-// Create a new webhook.
-//
-// HipChat API docs: https://www.hipchat.com/docs/apiv2/method/create_webhook
-func (r *RoomService) CreateWebhook(id interface{}, roomReq *CreateWebhookRequest) (*Webhook, *http.Response, error) {
-	req, err := r.client.NewRequest("POST", fmt.Sprintf("room/%v/webhook", id), roomReq)
-	if err != nil {
-		return nil, nil, err
-	}
-
-	wh := new(Webhook)
-
-	resp, err := r.client.Do(req, wh)
-	if err != nil {
-		return nil, resp, err
-	}
-
-	return wh, resp, nil
-}
-
 // History fetches a room's chat history.
 //
 // HipChat API docs: https://www.hipchat.com/docs/apiv2/method/view_room_history
